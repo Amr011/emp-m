5f74725117705c285dc83aedf408341766662aeb
Amr109 <grand.109.master@gmail.com>
2022-06-01 18:09:49 +0300

    reset database settings

--- config.php
+++ config.php
@@ -1,8 +1,8 @@
 <?php
-$servername = "f80b6byii2vwv8cx.chr7pe7iynqr.eu-west-1.rds.amazonaws.com";
-$username = "hfq7drwuaivebwpq";
-$password = "iy4a6omz2v3nk82k";
-$dbname = "oxna9dztwb3o0k8c";
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "mydb2";
 
 //Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
