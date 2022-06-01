944d8d4dc5a256973159d5a25fc1ad45525a8634
Amr109 <grand.109.master@gmail.com>
2022-06-01 18:06:02 +0300

    update database info

--- config.php
+++ config.php
@@ -1,8 +1,8 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "mydb2";
+$servername = "f80b6byii2vwv8cx.chr7pe7iynqr.eu-west-1.rds.amazonaws.com";
+$username = "hfq7drwuaivebwpq";
+$password = "iy4a6omz2v3nk82k";
+$dbname = "oxna9dztwb3o0k8c";
 
 //Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
